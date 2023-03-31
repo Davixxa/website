eb9684d61d462d888cdc43f57a2daf30378c34c4
Davixxa <serverboost100.dk@gmail.com>
2023-03-31 22:38:05 +0200

    more stuff

--- miawebsite/settings/production.py
+++ miawebsite/settings/production.py
@@ -3,6 +3,9 @@ from .base import *
 root = "/home/davix/davixxa.net/public"
 STATIC_ROOT = os.path.join(root, "static")
 MEDIA_ROOT = os.path.join(root, "media")
+ALLOWED_HOSTS = ["*"]
+SECRET_KEY = os.environ["SECRET_KEY"]
+
 
 DEBUG = False
 
